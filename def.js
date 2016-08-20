fb0dd4c9b376c9e8592e90c9fab796ac489ff1aa
ogewan <seun40@gmail.com>
2016-08-20 14:13:32 -0500

    init commit

---
+++ def.js
@@ -0,0 +1,28 @@
+(function (win){
+    function TekTekNeo(path)
+    {
+        var count = 0,
+            Item = function (name, type, src, data, ID, tID) {
+                this.name = name || "";
+                this.type = type || -1;
+                this.src = src || "";
+                this.data = data || {};
+                this.tID = tID || -1;
+                this.ID = ID || count;
+                count++;
+            },
+            inventory = {};
+        this.Make = function(name, type, src, data){
+            var item = new Item(name, type, src, data);
+            inventory.type = inventory.type || [];
+            item.tID = inventory.type.length;
+            inventory.type.push(item);
+        };
+        this.Init = function(path){
+
+        };
+    };
+    win.ttn = new TekTekNeo();
+    return window.addEventListener ? window.addEventListener("load", win.ttn.init, 0) : 
+     (window.attachEvent ? window.attachEvent("onload", win.ttn.init) : window.onload = win.ttn.init);
+})(window)
